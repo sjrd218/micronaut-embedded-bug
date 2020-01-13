f20313d2470294459d340a542f85293b98549c64
Stephen Joyner <stephen@rundeck.com>
2020-01-13 16:07:55 -0600

    Initial

---
+++ src/test/groovy/multi/start/embedded/bug/MyAbstractSpec.groovy
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 Rundeck, Inc. (http://rundeck.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package multi.start.embedded.bug
+
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Shared
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest
+class MyAbstractSpec extends Specification {
+
+    @Shared
+    @Inject
+    EmbeddedServer server
+
+    def setupSpec() {
+        println "do some setup stuff here"
+    }
+}
